db6fe3218888067fc5684ff0372a177582a24366
Youssef-Mahmoud-web <joshuman526@gmail.com>
2022-06-08 08:26:02 +0200

    Edit img Features

--- js/main.js
+++ js/main.js
@@ -3,18 +3,23 @@
 let mainTitleServ = document.querySelector(".services .main-title");
 let fadeServ = document.querySelector('.fade-serv')
 window.addEventListener("scroll", (eo) => {
-  if (window.scrollY >= 530) {
+  if (window.scrollY >= 510) {
     mainTitleServ.classList.add('animation-title')
     fadeServ.style.top = "0"
     fadeServ.style.opacity = "1"
   }
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
 });
 // 2-Features Animation
 let mainTitleFeat = document.querySelector(".features .main-title");
+let imgOpacity = document.querySelectorAll('.features .box-img-feat')
+console.log(imgOpacity);
 window.addEventListener("scroll", (eo) => {
   if (window.scrollY >= 900) {
     mainTitleFeat.classList.add('animation-title')
+    imgOpacity.forEach(item => {
+      item.style.opacity = '1'
+    });
   }
 })
 //------------------------------------------------------------
