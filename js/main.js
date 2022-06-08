336fd252ae2292146a43edf1fad890238185f9d4
Youssef-Mahmoud-web <joshuman526@gmail.com>
2022-06-08 08:17:35 +0200

    Add Features Section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-// Start Services Animation
-let mainTitle = document.querySelector(".main-title");
+// Start Animation 
+// 1-Services Animation
+let mainTitleServ = document.querySelector(".services .main-title");
 let fadeServ = document.querySelector('.fade-serv')
 window.addEventListener("scroll", (eo) => {
-  if (window.scrollY >= 550) {
-    mainTitle.classList.add('animation-title')
+  if (window.scrollY >= 530) {
+    mainTitleServ.classList.add('animation-title')
     fadeServ.style.top = "0"
     fadeServ.style.opacity = "1"
   }
-  // console.log(window.scrollY);
+  console.log(window.scrollY);
 });
+// 2-Features Animation
+let mainTitleFeat = document.querySelector(".features .main-title");
+window.addEventListener("scroll", (eo) => {
+  if (window.scrollY >= 900) {
+    mainTitleFeat.classList.add('animation-title')
+  }
+})
 //------------------------------------------------------------
